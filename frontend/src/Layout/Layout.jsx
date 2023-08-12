a410ca46a1d765af66959850862ae7d7f243ed49
im-light <72781778+omar49511@users.noreply.github.com>
2023-08-11 23:38:39 -0700

    creacion de carpeta frontend

---
+++ frontend/src/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Routers from "../routes/Routers";
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Routers />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
